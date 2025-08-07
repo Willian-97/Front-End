1d83e18306fd9629d76ecd4d02543b0e764ac691
Will.dev <wiiillian21@gmail.com>
2025-08-06 21:02:06 -0300

    Aulas sobre loops

---
+++ Aula7-loops/loops.js
@@ -0,0 +1,21 @@
+// For 
+for (let dia = 1; dia <= 30; dia++) {
+    console.log("Dia", dia);
+
+}
+
+
+// while 
+let dia = 1;
+while (dia <= 30) {
+    console.log("Dia", dia);
+    dia++;
+
+}
+
+// do while
+do {
+    console.log("Dia", dia);
+    dia++ 
+}while(dia <= 30)
+
